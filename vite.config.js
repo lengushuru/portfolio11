36495ad383a7350b98257ce5f1f6610cb8247f32
rails --v <ruby -v>
2023-09-01 20:18:21 +0300

    Fix images missing problem

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { copy } from 'vite-plugin-copy'; // Import the 'copy' function specifically
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    copy({
+      patterns: [
+        { from: 'src/images', to: 'dist/images' }, // Adjust the source and destination paths as needed
+      ],
+    }),
+  ],
+});
